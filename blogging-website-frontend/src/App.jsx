dcf0aa6b8fdc87685966c1a1ad5f0acb5ad1a9c0
sanu-pc <sethisanuja1998.com>
2024-05-26 17:25:42 +0530

    user auth form layout

--- blogging-website-frontend/src/App.jsx
+++ blogging-website-frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/navbar.component";
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import UserAuthForm from "./pages/userAuthForm.page";
+
 
 const App = () => {
     return (
        <>
        <Routes>
         <Route path="/" element={<Navbar/>}>
-        <Route path="signin" element={<h1>This is sign in page</h1>}/>
-        <Route path="signup" element={<h1>This is sign up page</h1>}/>
+        <Route path="signin" element={<UserAuthForm type={"sign-in"}/>}/>
+        <Route path="signup" element={<UserAuthForm type={"sign-up"}/>}/>
         </Route>
        </Routes>
        
---
+++ blogging-website-frontend/src/pages/userAuthForm.page.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import InputBox from '../components/input.component'
+
+const userAuthForm = ({type}) => {
+  return (
+    <section className='h-cover flex items-center justify-center'>
+        <form  className="w-[80%] max-w-[400px]">
+            <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+                {type=="sign-in"?"Welcome back":"Join us today"}
+            </h1>
+            {
+                type != "sign-in"?
+                <InputBox
+                 name="fullname"
+                type="fullname"
+                placeholder="Full Name"
+                icon={"fi-rr-user"}/>:""
+            }
+             
+               
+                <InputBox
+                 name="email"
+                type="email"
+                placeholder="Email"
+                icon={"fi-rr-envelope"}/>
+            
+              
+               
+               <InputBox
+                name="password"
+               type="password"
+               placeholder="Password"
+               icon={"fi-rr-key"}/>
+               <button className="btn-dark center mt-14">{type.replace("-"," ")}</button>
+           
+        </form>
+    </section>
+  )
+}
+
+export default userAuthForm
