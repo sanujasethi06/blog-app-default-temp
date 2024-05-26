1a37fa26caffd7697a0f061b2426f12872fba9ba
sanu-pc <sethisanuja1998.com>
2024-05-27 00:30:21 +0530

    form animation with routing

--- blogging-website-frontend/src/pages/userAuthForm.page.jsx
+++ blogging-website-frontend/src/pages/userAuthForm.page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import InputBox from '../components/input.component'
+import { Link } from 'react-router-dom';
+import AnimationWrapper from '../common/page-animation';
+import InputBox from '../components/input.component';
+import googleIcon from "../imgs/google.png"
 
 const userAuthForm = ({type}) => {
   return (
+    <AnimationWrapper keyValue={type}>
     <section className='h-cover flex items-center justify-center'>
         <form  className="w-[80%] max-w-[400px]">
             <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
@@ -31,10 +35,30 @@ const userAuthForm = ({type}) => {
                type="password"
                placeholder="Password"
                icon={"fi-rr-key"}/>
-               <button className="btn-dark center mt-14">{type.replace("-"," ")}</button>
-           
+               <button className="btn-dark center mt-14" type='submit'>{type.replace("-"," ")}</button>
+
+               <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+                <hr className="w-1/2 border-black" />
+                <p className="">or</p>
+                <hr className="w-1/2 border-black" />
+               </div>
+               <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+                <img src={googleIcon} alt="" className="w-5" />
+                Continue with google
+               </button>
+           {
+            type=="sign-in"?
+            <p className="mt-6 text-dark-grey text-xl text-center">
+                Dont't have an account ?
+                <Link to="/signup" className="underline text-black text-xl ml-1">Join us today</Link>
+            </p>: <p className="mt-6 text-dark-grey text-xl text-center">
+                Already a member ?
+                <Link to="/signin" className="underline text-black text-xl ml-1">Sign in here</Link>
+            </p>
+           }
         </form>
     </section>
+    </AnimationWrapper>
   )
 }
 
