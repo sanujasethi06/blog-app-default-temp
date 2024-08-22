5346a51398361894b5543b7421e3e7a69afcf172
sanu-pc <sethisanuja1998.com>
2024-08-22 13:18:56 +0530

    form submission data push to backend with validation

--- blogging-website-frontend/src/pages/userAuthForm.page.jsx
+++ blogging-website-frontend/src/pages/userAuthForm.page.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Axios from 'axios'
 import { Link } from 'react-router-dom';
 import AnimationWrapper from '../common/page-animation';
 import InputBox from '../components/input.component';
 import googleIcon from "../imgs/google.png"
-
+import {Toaster,toast} from "react-hot-toast"
 const userAuthForm = ({type}) => {
+    const authForm = useRef();
+  const userAuthThroughServer=(serverRoute,formData)=>{
+        Axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute,formData)
+        .then(({data})=>{
+            console.log(data)
+        }
+            
+        )
+        .catch(({response})=>{
+            toast.error(response.data.error)
+        }
+
+        )
+
+    }
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        let serverRoute= type =="sign-in"? "/signin":"/signup" 
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password 
+        //formData
+        let form = new FormData(authForm.current)
+        let formData ={};
+        for (let [key,value] of form.entries()){
+            formData[key]=value;
+        }
+        
+        let {fullname,email,password} = formData
+        //form validation
+        if(fullname){
+            if(fullname.length<3){
+                return toast.error("fullname must be atleast 3 letteers long")
+               }
+        }
+        
+           if(!email.length){
+            return toast.error("Enter email")
+           }
+           if(!emailRegex.test(email)){
+            return toast.error("Email is invalid")
+           }
+           if(!passwordRegex.test(password)){
+            return toast.error("Password should be 6 to 20 characters long with a numeric 1 uppercase and 1 lowercase")
+           }
+           userAuthThroughServer(serverRoute,formData);
+          
+    }
   return (
     <AnimationWrapper keyValue={type}>
     <section className='h-cover flex items-center justify-center'>
-        <form  className="w-[80%] max-w-[400px]">
+        <Toaster/>
+        <form  ref={authForm} onSubmit={handleSubmit} className="w-[80%] max-w-[400px]">
             <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
                 {type=="sign-in"?"Welcome back":"Join us today"}
             </h1>
