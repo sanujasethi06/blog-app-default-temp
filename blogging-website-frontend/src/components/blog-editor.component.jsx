c41c34130f72159e87a301d3b720fa20d7f8e5b1
sanu-pc <sethisanuja1998.com>
2024-09-25 23:34:40 +0530

    Editor page pubhish content

--- blogging-website-frontend/src/components/blog-editor.component.jsx
+++ blogging-website-frontend/src/components/blog-editor.component.jsx
@@ -1,37 +1,119 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
 import AnimationWrapper from '../common/page-animation';
 import logo from "../imgs/logo.png"
 import defaultBanner from "../imgs/blog banner.png"
+import { uploadimage } from '../common/aws';
+import { useRef } from 'react';
+import {Toaster,toast} from 'react-hot-toast'
+import { EditorContext } from '../pages/editor.pages';
+import { useEffect } from 'react';
+import EditorJS from '@editorjs/editorjs'
+import { tools } from './tools.component';
 
 const Editor = () => {
-    
+    let {blog,blog:{title,banner,content,tags,desc},setBlog,textEditor,setTextEditor,setEditorState}=useContext(EditorContext)
+
+    useEffect(()=>{
+        setTextEditor(new EditorJS({
+            holderId:"textEditor",
+            data:'',
+            tools:tools,
+            placeholder:"Let's write an awesome story"
+        }))
+    },[])
+
+    console.log(blog)
+    // let blogBannerRef=useRef();
     const handlebannerupload=(e)=>{
         let img= e.target.files[0];
-        console.log(img)
+        // console.log(img)
+
+        if (img){
+
+            let loadingToast = toast.loading("Uploading...")
+            uploadimage(img).then((url)=>{
+                toast.dismiss(loadingToast);
+                toast.success("Uploaded Successfully")
+                // console.log(blogBannerRef.current)
+                if(url){
+                // blogBannerRef.current.src=url;
+                setBlog({...blog,banner:url})
+                }
+            }).catch((err)=>{
+                toast.dismiss(loadingToast);
+                return toast.error(err);
+            })
+        }
+       
     }
+
+    const handleTitleKeyDown=(e)=>{
+        console.log(e)
+        if(e.keyCode==13){
+            e.preventDefault();
+        }
+    }
+//Title text area height dynamic
+    const handleTitleChange=(e)=>{
+        let input=e.target;
+        input.style.height='auto';
+        input.style.height=input.scrollHeight + 'px';
+
+        setBlog({...blog,title:input.value})
+    }
+    const handleError=(e)=>{
+        let img=e.target;
+        img.src=defaultBanner;
+    }
+    const handlePublishEvent=()=>{
+        if(!banner.length){
+            return toast.error("Upload a blog banner to publish it")
+        }
+        if(!title.length){
+            return toast.error("Write blog title to publish it")
+        }
+        if(textEditor.isReady){
+            textEditor.save().then(data=>{
+                if(data.blocks.length){
+                    setBlog({...blog,content:data});
+                    setEditorState("publish")
+                }else{
+                    return toast.error("Write something in your blog to publish it")
+                }
+            }).catch(err=>{
+                console.log(err)
+            })
+        }
+    }
+
   return (
     <>
     <nav className="navbar">
         <Link to="/" className='flex-none w-10'>
             <img src={logo} alt="" className="" />
         </Link>
-        <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+        <p className="max-md:hidden text-black line-clamp-1 w-full">{title.length? title: "New Blog"}</p>
         <div className='flex gap-4 ml-auto'>
-            <button className="btn-dark py-2">Publish</button>
+            <button className="btn-dark py-2" onClick={handlePublishEvent}>Publish</button>
             <button className="btn-light py-2"> Save Draft</button>
         </div>
     </nav>
+    <Toaster/>
     <AnimationWrapper>
         <section>
         <div className="mx-auto max-w-[900px] w-full">
             <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
                 <label htmlFor="uploadBanner">
-                    <img src={defaultBanner} alt="" className="z-20"  />
+                    <img src={banner} alt="" className="z-20" onError={handleError}/>
                     <input id="uploadBanner" type="file" 
                     accept='.png, .jpg, .jpeg' hidden onChange={handlebannerupload}/>
                 </label>
             </div>
+
+            <textarea name="" id="" placeholder='Blog Title' className='text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40' onKeyDown={handleTitleKeyDown} onChange={handleTitleChange}></textarea>
+            <hr className="w-full opacity-10 my-5" />
+            <div id="textEditor" className="font-gelasio"></div>
         </div>
         </section>
        
--- blogging-website-frontend/src/pages/editor.pages.jsx
+++ blogging-website-frontend/src/pages/editor.pages.jsx
@@ -1,15 +1,34 @@
-import React, { useContext, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import { Navigate } from 'react-router-dom'
 import { UserContext } from '../App'
 import Editor from '../components/blog-editor.component'
 import PublishForm from '../components/publish-form.component'
 
+const blogStructure={
+  title:'',
+  banner:'',
+  content:[],
+  tags:'',
+  desc:'',
+  author:{personal_info:{}}
+}
+
+export const EditorContext = createContext({ });
+
+
 const EditorPage = () => {
-    const [editorState,setEditorState]=useState("editor")
+  const [blog,setBlog]=useState(blogStructure)
+    const [editorState,setEditorState]=useState("editor");
+    const [textEditor,setTextEditor]=useState({isReady:false});
+
     const {userAuth :{access_token}} = useContext(UserContext)
   return (
-    access_token===null? <Navigate to={"/signin"}/>
-    : editorState == "editor" ? <Editor/>: <PublishForm/>
+    <EditorContext.Provider value={{blog,setBlog,editorState,setEditorState,textEditor,setTextEditor}}>
+        {
+      access_token===null? <Navigate to={"/signin"}/>
+      : editorState == "editor" ? <Editor/>: <PublishForm/>
+        }
+    </EditorContext.Provider>
   )
 }
 
