31a5a47414f3638a4274824caaf5975131b5cbc7
sanu-pc <sethisanuja1998.com>
2024-10-09 17:35:21 +0530

    publish blog contents to database -client-side

--- blogging-website-frontend/src/components/publish-form.component.jsx
+++ blogging-website-frontend/src/components/publish-form.component.jsx
@@ -1,5 +1,8 @@
+import axios from 'axios'
 import React, { useContext } from 'react'
 import toast, { Toaster } from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../App'
 import AnimationWrapper from '../common/page-animation'
 import { EditorContext } from '../pages/editor.pages'
 import Tag from './tags.component'
@@ -7,10 +10,14 @@ import Tag from './tags.component'
 const PublishForm = () => {
   let characterLimit=200;
   let tagLimit=10;
-  let {blog,blog:{banner,title,tags,desc},setEditorState,setBlog}=useContext(EditorContext)
+  let {blog,blog:{banner,title,tags,desc,content},setEditorState,setBlog}=useContext(EditorContext)
   const handleCloseEvent=()=>{
     setEditorState("editor")
   }
+
+  const {userAuth :{access_token}} = useContext(UserContext)
+
+  let navigate = useNavigate();
 
   const handleBlogTitleChange=(e)=>{
     let input =e.target;
@@ -41,6 +48,51 @@ const handleKeyDown=(e)=>{
     )
     e.target.value="";
   }
+}
+
+const publishBlog=(e)=>{
+
+  if(e.target.className.includes("disable")){
+    return;
+  }
+  if(!title.length){
+    return toast.error("Write blog title before publishing")
+  }
+  if(!desc.length || desc.length>characterLimit){
+    return toast.error("Write a description about your blog within 200 characters")
+  }
+  if(!tags.length){
+    return toast.error("Enter atleast 1 tag to help us to rank your blog")
+  }
+
+  let loadingToast= toast.loading("Publishing...");
+  e.target.classList.add('disable')
+
+  let blogObj ={
+    title,banner,desc,content,tags,draft:false
+  }
+
+  axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/create-blog",blogObj,{
+    headers:{
+      'Authorization':`Bearer ${access_token}`
+    }
+  })
+  .then(()=>{
+    e.target.classList.remove("disable");
+    toast.dismiss(loadingToast);
+    toast.success("Published !")
+
+    setTimeout(()=>{
+      navigate("/")
+    },500)
+  })
+  .catch(({response})=>{
+    e.target.classList.remove("disable");
+    toast.dismiss(loadingToast);
+
+    return toast.error(response.data.error)
+  })
+
 }
   return (
     <AnimationWrapper>
@@ -78,7 +130,7 @@ const handleKeyDown=(e)=>{
         
         </div>
         <p className="mt-1 mb-4 text-dark-grey text-right">{tagLimit - tags.length} Tags left</p>
-        <button className="btn-dark px-8">Publish</button>
+        <button className="btn-dark px-8" onClick={publishBlog}>Publish</button>
       </div>
    </section>
     </AnimationWrapper>
