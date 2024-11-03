deeb0b29f23b6b8d79f4ab2fd0b92a93aad38171
sanu-pc <sethisanuja1998.com>
2024-11-04 00:09:28 +0530

    blog interaction section page second commit

--- server/server.js
+++ server/server.js
@@ -253,17 +253,17 @@ server.get("/trending-blogs",(req,res)=>{
     })
 })
 server.post("/search-blogs",(req,res)=>{
-    let {tag ,query,author,page}=req.body;
+    let {tag ,query,author,page,limit,eliminate_blog}=req.body;
 
     let findQuery;
     if(tag){
-        findQuery={tags:tag,draft:false};
+        findQuery={tags:tag,draft:false, blog_id : {$ne: eliminate_blog}};
     }else if(query){
         findQuery={draft: false,title:new RegExp(query,'i')}
     }else if(author){
         findQuery={author,draft:false}
     }
-    let maxLimit=5;
+    let maxLimit=limit ? limit : 2 
     Blog.find(findQuery)
     .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id") // Remove space after hyphen
     .sort({ "publishedAt": -1 })
